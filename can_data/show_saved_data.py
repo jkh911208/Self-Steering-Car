e899427df7144cdfb25680d95b2303417eee820d
James Jeon <jkh911208@naver.com>
2017-07-28 10:07:50 -0500

    new line

--- can_data/show_saved_data.py
+++ can_data/show_saved_data.py
@@ -1,12 +1,13 @@
 import numpy as np 
 import time
 
-f = open("test.txt","w")
+f = open("left_to_right.txt","w")
 loaded_data = np.load("left_to_right.npy")
 
 for i in loaded_data:
 	msg = i
 	f.write(msg)
+	f.write('\n')
 	print(msg)
 	#time.sleep(0.5)
 
