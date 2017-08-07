69adf9e5a45c03379dae93e009ba7e26d30d5734
James Jeon <jkh911208@naver.com>
2017-08-07 00:12:31 -0500

    almost finish with balancing data

--- balance_data.py
+++ balance_data.py
@@ -1,79 +1,23 @@
 import os
 import numpy as np
+import time
 from random import shuffle
 # os.path.exists("/etc/password.txt")
-'''
 
-# amount of lists you want to create
-for i in range(0,16):
-	plus = "" # this line is here to clear out the previous command
-	plus = "plus" + str(i) + " = []"
-	minus = "" # this line is here to clear out the previous command
-	minus = "minus" + str(i) + " = []"
-	exec(plus)
-	exec(minus)
-
-print(plus5)
-
-for i in range(0,16):
-	plus = "plus{}".format(i)
-	print(list(plus))
-'''
+data_size = 500
 minus = []
-plus =
+plus = []
 for i in range(0,16):
-	minus[i] = []
-	plis[i] = []
-
-
-print(minus, plus)
-
-'''
-plus_15 = []
-plus_14 = []
-plus_13 = []
-plus_12 = []
-plus_11 = []
-plus_10 = []
-plus_9 = []
-plus_8 = []
-plus_7 = []
-plus_6 = []
-plus_5 = []
-plus_4 = []
-plus_3 = []
-plus_2 = []
-plus_1 = []
-plus_0 = []
-minus_0 = []
-minus_1 = []
-minus_2 = []
-minus_3 = []
-minus_4 = []
-minus_5 = []
-minus_6 = []
-minus_7 = []
-minus_8 = []
-minus_9 = []
-minus_10 = []
-minus_11 = []
-minus_12 = []
-minus_13 = []
-minus_14 = []
-minus_15 = []
-'''
-
-'''
-print(data_set)
+	minus.append([])
+	plus.append([])
 
 file_list = os.listdir('/raw_data')
 #print(file_list)
 for file_name in file_list:
 	if file_name.endswith('.npy'):
-		# print(file_name)
+		print(file_name)
 		file_location = "/raw_data/" + file_name
 		loaded_data = np.load(file_location)
-
 
 		for data in loaded_data:
 			# change the can data (HEX) to numerical data
@@ -87,28 +31,110 @@ for file_name in file_list:
 			if(int_data > 550):
 				int_data = int_data - 4095
 				int_decimal = 1 - int_decimal 
-				final_data = int_data - int_decimal
+				steering_angle = int_data - int_decimal
 			else:
 				# put the int and the decimal together
-				final_data = int_data + int_decimal
+				steering_angle = int_data + int_decimal
 			
-			int_data = int(final_data)
+			int_data = int(steering_angle)
+
+			if int_data >= 16 or int_data <= -16:
+				continue
+
 			if int_data == 0:
-				if int_data > final_data:
-					data_set['-0'] = data_set['-0'] + 1
+				if int_data > steering_angle:
+					minus[int_data].append([data])
 				else:
-					data_set['0'] = data_set['0'] + 1
+					plus[int_data].append([data])
+			elif int_data > 0:
+				plus[int_data].append([data])
 			else:
-				string = str(int_data)
-				# print(string)
-				data_set[string] = data_set[string] + 1
+				int_data = int_data * -1
+				minus[int_data].append([data])
+for i in range(0,16):
+	shuffle(minus[i])
+	shuffle(plus[i]) 
 
-for i in range(-20,21):
-	if i == 0:
-		print("data_set[-0] = ", data_set['-0'])
-	string = str(i)
-	print("data_set[{}] = ".format(string), data_set[string])
+	minus[i] = minus[i][:data_size]
+	plus[i] = plus[i][:data_size]
+
+for i in range(0,16):
+	if len(minus[i]) != data_size:
+		while True:
+			minus[i].append(minus[i])
+			if len(minus[i]) >= data_size:
+				break
+	if len(plus[i]) != data_size:
+		while True:
+			plus[i].append(plus[i])
+			if len(plus[i]) >= data_size:
+				break
+
+for i in range(0,16):
+	shuffle(minus[i])
+	shuffle(plus[i]) 
+
+	minus[i] = minus[i][:data_size]
+	plus[i] = plus[i][:data_size]
+
+
+for i in range(0,16):
+	print("minus[{}] :".format(i),len(minus[i]),"plus[{}] :".format(i), len(plus[i]))
+
+batch_size = 100
+batch = int(data_size / batch_size)
+#print(data_size/batch_size , batch)
+for j in range(batch):
+	temp = []
+	for i in range(0,16):
+		temp_minus = minus[i][j*batch_size:(j+1)*batch_size]
+		#print("temp_minus : " , len(temp_minus))
+		temp_plus = plus[i][j*batch_size:(j+1)*batch_size]
+		#print("temp_plus : ", len(temp_plus))
+
+		for y in range(len(temp_minus)):
+			temp.append([temp_minus[y]])
+			temp.append([temp_plus[y]])
+
+		#print("temp : ", len(temp))
+
+	name = str(time.time())
+	file_name = "/raw_data/balanced_data/" + name + ".npy"
+	#print(len(temp))	
+	np.save(file_name,temp)
+	print("Saved file name : ",name + ".npy")
 
 
 
-'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
