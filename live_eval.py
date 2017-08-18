fb331a52789677d8873d2be46c9f942fd51cf496
James Jeon <jkh911208@naver.com>
2017-08-18 14:55:43 -0500

    project done

--- live_eval.py
+++ live_eval.py
@@ -6,6 +6,9 @@ import model
 import params
 import cv2
 import numpy as np
+import can
+
+bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
 
 cap = cv2.VideoCapture(0)
 cap.set(3,300) # set height
@@ -28,21 +31,21 @@ def get_frame():
 	return img, frame
 
 def get_angle():
-    while(True):
-	# get one can data
-        can_data = str(bus.recv())
-	# check it is steering angle, if yes then stop the loop
-        if(can_data.find("ID: 0025") > 0):
-            break
-
-    # change the can data (HEX) to numerical data
+	while(True):
+		# get one can data
+		can_data = str(bus.recv())
+		# check it is steering angle, if yes then stop the loop
+		if(can_data.find("ID: 0025") > 0):
+			break
+	
+	# change the can data (HEX) to numerical data
 	tmp = can_data
 	hex_data = tmp[-23:-21] + tmp[-20:-18]
 	hex_decimal = tmp[-3:-1]
 	int_data = int(hex_data, 16)
 	int_decimal = int(hex_decimal, 16) / 256
 
-			# if the steering wheel angle in in right to the center
+	# if the steering wheel angle in in right to the center
 	if(int_data > 550):
 		int_data = int_data - 4096
 		int_decimal = 1 - int_decimal 
@@ -51,7 +54,7 @@ def get_angle():
 		# put the int and the decimal together
 		final_data = int_data + int_decimal
 
-    return final_data
+	return final_data
 
 def main():
 	# discard first 40 frames to give time for webcam to get the proper exposure
@@ -63,21 +66,22 @@ def main():
 	while True:
 		angle = get_angle()
 		img, frame = get_frame()
-
+		frame = cv2.flip(frame,0)
+		frame = cv2.flip(frame,1)
 		deg = model.y.eval(feed_dict={model.x: [img], model.keep_prob: 1.0})[0][0]
-		difference = ((final_data+16)-(deg+16))/(final_data+16)
-		if difference < 0:
-			difference = difference * -1
-		difference = difference * 100
+		#difference = ((final_data+16)-(deg+16))/(final_data+16)
+		#if difference < 0:
+		#	difference = difference * -1
+		#difference = difference * 100
 
 		predicted = "predicted : " + str(deg)
-		actual = "actual : " + str(final_data)
-		percent = "% difference : " + str(difference)
+		actual = "actual : " + str(angle)
+		#percent = "% difference : " + str(difference)
 
-		cv2.putText(image,predicted,(10,30), cv2.FONT_HERSHEY_COMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
-		cv2.putText(image,actual,(50,30), cv2.FONT_HERSHEY_COMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
-		cv2.putText(image,percent,(70,30), cv2.FONT_HERSHEY_COMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
-		
+		cv2.putText(frame,predicted,(10,30), cv2.FONT_HERSHEY_COMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
+		cv2.putText(frame,actual,(10,60), cv2.FONT_HERSHEY_COMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
+		#cv2.putText(frame,percent,(70,30), cv2.FONT_HERSHEY_COMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
+		print(deg,angle)		
 		cv2.imshow('frame', frame)
 
 		if cv2.waitKey(1) & 0xFF == ord('q'):
